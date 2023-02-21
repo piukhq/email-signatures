f8024af823d4d71293af0bde3adfdc44dc71ea0c
Chris Pressland <mail@cpressland.io>
2023-02-21 17:25:19 +0000

    Updated email sig

--- gen.js
+++ gen.js
@@ -35,30 +35,46 @@ var phone = app.displayDialog("What's your Phone Number?", {
 template = `
 <br />
 <table width="100%" style="border-spacing: 0px">
-  <tr>
-    <td><a href="https://bink.com/"><img alt='Bink' title='Bink' width="150" height="114" src="https://api.gb.bink.com/content/media/logos/bink_with_text_email_signature.png"></a></td>
-  </tr>
-  <tr>
-    <td style="font-family: Arial, Helvetica, sans-serif; color: #8EB1B7; padding: 0px; font-size: 20px; font-weight: bold; white-space: nowrap;">#name#</td>
-  </tr>
-  <tr>
-    <td style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #8EB1B7; padding: 0px; padding-bottom: 5px; white-space: nowrap;">#title#</td>
-  </tr>
-  <tr>
-    <td style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #8EB1B7; padding: 0px;">#phone#</td>
-  </tr>
-  <tr>
-    <td style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; padding: 0px; padding-top: 5px;"><a href="https://bink.com/" style="color: #CCDBDF; text-decoration: none;">bink.com</a></td>
-  </tr>
-  <tr>
-    <td width="100%" colspan="2"  style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; padding: 0px; padding-top: 5px;">
-      <p style="color: #8EB1B7;">
-        <a href="https://www.linkedin.com/company/bink" style="text-decoration:none; font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #CCDBDF;">LinkedIn</a>&nbsp;&nbsp;|&nbsp;
-        <a href="https://www.facebook.com/BinkLoyalty" style="text-decoration:none; font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #CCDBDF;">Facebook</a>&nbsp;&nbsp;|&nbsp;
-        <a href="https://www.instagram.com/bink_loyalty/" style="text-decoration:none; font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #CCDBDF;">Instagram</a>
-      </p>
-    </td>
-  </tr>
+    <tr>
+        <td>
+            <a href="https://bink.com/">
+            <img alt='Bink' title='Bink' width="250" style="margin-bottom:30px" src="https://api.gb.bink.com/content/logos/bink-mono-teal_resize_2023.png">
+            </a>
+        </td>
+    </tr>
+    
+    <tr>
+        <td style="font-family: Arial, Helvetica, sans-serif; color: #30AFB9; font-size: 26px; font-weight: 500; white-space: nowrap;">#name#</td>
+    </tr>
+    
+    <tr>
+        <td style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; letter-spacing: 0.8 px; color: #30AFB9; padding-bottom: 10px; white-space: nowrap;">#title#</td>
+    </tr>
+    
+    <tr>
+        <td style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #30AFB9; padding-bottom: 5px;">#phone#</td>
+    </tr>
+    
+    <tr>
+        <td style="font-size: 14px; font-family: Arial, Helvetica, sans-serif;  padding-bottom: 10px;">
+            <a href="https://bink.com/" style="color: #30AFB9; text-decoration: none">bink.com</a>
+        </td>
+    </tr>
+    
+    <tr>
+        <td width="100%" colspan="2"  style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; padding: 0px; padding-top: 5px;">
+            <a href="https://uk.linkedin.com/company/bink" style="text-decoration:none; font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #30AFB9;">
+                <img alt='Linkedin' title='Linkedin' width="30" src="https://api.gb.bink.com/content/logos/linkdn_2023.png">
+            </a>&nbsp;
+            <a href="https://www.facebook.com/BinkLoyalty" style="text-decoration:none; font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #30AFB9;">
+                <img alt='Facebook' title='Facebook' width="30" src="https://api.gb.bink.com/content/logos/fb_2023.png">
+            </a>&nbsp;
+            <a href="https://www.instagram.com/bink_loyalty" style="text-decoration:none; font-size: 14px; font-family: Arial, Helvetica, sans-serif; color: #30AFB9;">
+                <img alt='Instagram' title='Instagram' width="30" src="https://api.gb.bink.com/content/logos/insta_2023.png">
+            </a>
+        </td>
+    </tr>
+
 </table>
 `
 
